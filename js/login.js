45d61328b66eb0dc983b44951aa891583b976988
admin <cyk5539@gmail.com>
2024-05-08 17:02:34 +0900

    11123

--- js/login.js
+++ js/login.js
@@ -15,6 +15,7 @@ const check_xss = (input) => {
     
 const check_input = () => 
 {
+    const idsave_check = document.getElementById('idSavecheck');
     const loginForm = document.getElementById('login_form');
     const loginBtn = document.getElementById('login_btn');
     const emailInput = document.getElementById('typeEmailX');
@@ -23,6 +24,7 @@ const check_input = () =>
     alert(c);
     const emailValue = emailInput.value.trim();
     const passwordValue = passwordInput.value.trim();
+    
     if (emailValue === '') 
     {
         alert('이메일을 입력하세요.');
@@ -69,5 +71,40 @@ const check_input = () =>
     console.log('이메일:', emailValue);
     console.log('비밀번호:', passwordValue);
     loginForm.submit();
+    if(idsave_check.checked == true) 
+    { // 아이디 체크 o
+        alert("쿠키를 저장합니다.", emailValue);
+        setCookie("id", emailValue, 1); // 1일 저장
+        alert("쿠키 값 :" + emailValue);
+    }
+    else
+    { // 아이디 체크 x
+        setCookie("id", emailValue.value, 0); //날짜를 0 - 쿠키 삭제
+    }
+    function setCookie(name, value, expiredays) 
+    {
+        var date = new Date();
+        date.setDate(date.getDate() + expiredays);
+        document.cookie = escape(name) + "=" + escape(value) + "; expires=" + date.toUTCString() + ";path=/" + ";SameSite=None; Secure";
+    }
+    function getCookie(name) 
+    {
+        var cookie = document.cookie;
+        console.log("쿠키를 요청합니다.");
+        if (cookie != "") 
+        {
+            var cookie_array = cookie.split("; ");
+            for ( var index in cookie_array) 
+            {
+                var cookie_name = cookie_array[index].split("=");
+                if (cookie_name[0] == "id") 
+                {
+                    return cookie_name[1];
+                }
+            }
+        }
+    }
 };
-    document.getElementById("login_btn").addEventListener('click', check_input);
+
+
+document.getElementById("login_btn").addEventListener('click', check_input);
--- js/pop_up.js
+++ js/pop_up.js
@@ -1,6 +1,33 @@
 function pop_up() 
 {
-    window.open("../popup/popup.html", "팝업테스트", "width=400, height=300, top=15, left=15");
+    var cookieCheck = getCookie("popupYN");
+    if (cookieCheck != "N"){
+    window.open("../popup/popup.html", "팝업테스트", "width=400, height=300, top=10, left=10");
+    }
+}
+function setCookie(name, value, expiredays) 
+{
+    var date = new Date();
+    date.setDate(date.getDate() + expiredays);
+    document.cookie = escape(name) + "=" + escape(value) + "; expires=" + date.toUTCString() + ";path=/" + ";SameSite=None; Secure";
+}
+function getCookie(name) 
+{
+    var cookie = document.cookie;
+    console.log("쿠키를 요청합니다.");
+    if (cookie != "") 
+    {
+        var cookie_array = cookie.split("; ");
+        for ( var index in cookie_array) 
+        {
+            var cookie_name = cookie_array[index].split("=");
+            if (cookie_name[0] == "popupYN") 
+            {
+                return cookie_name[1];
+            }
+        }
+    }
+    return ;
 }
 function show_clock()
 {
@@ -26,3 +53,13 @@ function show_clock()
     }
     setTimeout(show_clock, 1000); //1초마다 갱신
 }
+function closePopup() 
+{
+    if (document.getElementById('check_popup').value) 
+    {
+        setCookie("popupYN", "N", 1);
+        console.log("쿠키를 설정합니다.");
+        self.close();
+    }
+}
+    
